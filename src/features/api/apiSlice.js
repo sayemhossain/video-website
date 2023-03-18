96d8d32ea37f240ca5f263e7fe2557e5c202052f
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-03-19 01:16:49 +0600

    creating api slice in feature

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,15 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-    reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:9000",
-    }),
-    endpoints: (builder) => ({
-        getVideos: builder.query({
-            query: () => "/videos",
-        }),
-    }),
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:9000",
+  }),
 });
-
-export const { useGetVideosQuery } = apiSlice;
