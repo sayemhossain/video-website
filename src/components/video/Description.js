f317357972b578b44dcfa139d9b351f5063780f0
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-03-19 02:29:26 +0600

    single video rendering done

--- src/components/video/Description.js
+++ src/components/video/Description.js
@@ -17,9 +17,11 @@ export default function Description({ video }) {
         <div className="flex gap-6 w-full justify-end">
           <div className="flex gap-1">
             <div className="shrink-0">
-              <img className="w-5 block" src={editImage} alt="Edit" />
+              <Link to={`/videos/edit/${video.id}`}>
+                <img className="w-5 block" src={editImage} alt="Edit" />
+              </Link>
             </div>
-            <Link to="/videos/edit/1">
+            <Link to={`/videos/edit/${video.id}`}>
               <span className="text-sm leading-[1.7142857] text-slate-600 cursor-pointer">
                 Edit
               </span>
