35273e256e65ac2635e97b1368f4e5a1eefa77d6
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-03-19 01:57:52 +0600

    get video api called and get data succefully

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,32 +1,26 @@
 import { useGetVideosQuery } from "../../features/api/apiSlice";
-import Error from "../ui/Error";
 import VideoLoader from "../ui/loaders/VideoLoader";
+import Error from "../ui/Error";
 import Video from "./Video";
 
 export default function Videos() {
-  // const { data: videos, isLoading, isError } = useGetVideosQuery();
+  const { data: videos, isLoading, isError } = useGetVideosQuery();
 
-  // decide what to render
+  //decide what to render
   let content = null;
 
-  <>
-    <VideoLoader />
-    <VideoLoader />
-    <VideoLoader />
-    <VideoLoader />
-  </>;
-
-  //   if (!isLoading && isError) {
-  //     content = <Error message="There was an error" />;
-  //   }
-
-  //   if (!isLoading && !isError && videos?.length === 0) {
-  //     content = <Error message="No videos found!" />;
-  //   }
-
-  //   if (!isLoading && !isError && videos?.length > 0) {
-  //     content = videos.map((video) => <Video key={video.id} video={video} />);
-  //   }
+  if (isLoading) {
+    content = <VideoLoader></VideoLoader>;
+  }
+  if (!isLoading && isError) {
+    content = <Error nessage="There was an error" />;
+  }
+  if (!isLoading && !isError && videos.length) {
+    content = <Error message="No videos found !" />;
+  }
+  if (!isLoading && !isError && videos.length > 0) {
+    content = videos.map((video) => <Video key={video.id} video={video} />);
+  }
 
   return content;
 }
