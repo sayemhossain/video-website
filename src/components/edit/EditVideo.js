c369664fc5ec8f69143ec57d6c9b482a6b1ab42c
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-03-24 00:58:04 +0600

    new code added

--- src/components/edit/EditVideo.js
+++ src/components/edit/EditVideo.js
@@ -19,6 +19,7 @@ export default function EditVideo() {
   if (!isLoading && !isError && video?.id) {
     content = <Form video={video} />;
   }
+
   return (
     <div className="max-w-7xl mx-auto px-5 lg:px-0">
       <div className="w-full">
